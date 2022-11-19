8ff3ed5230208b295d411147ad24fdacae34d77d
vituchini <petrovichvictorpro99@gmail.com>
2022-11-19 10:34:47 +0200

    feat: add removeTaskButtonX
    (cherry picked from commit ec9e8172e1405fe7fafbdcad3cef4a831775d8e2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Header} from './components/Header';
 import {v1} from 'uuid';
@@ -6,15 +6,22 @@ import {Todolist} from './Todolist';
 
 function App() {
 
-    let tasks = [
+    let [tasks, setTasks] = useState([
         {id: v1(), name: 'HTML&CSS', isDone: true},
         {id: v1(), name: 'JS', isDone: true},
         {id: v1(), name: 'React', isDone: false},
-    ]
+    ])
+
+    function removeTask(id: string) {
+        let filteredTasks = tasks.filter(t => t.id !== id)
+        setTasks(filteredTasks)
+    }
 
     return (
         <div className="App">
-            <Todolist tasks={tasks}  />
+            <Todolist tasks={tasks}
+                      removeTask={removeTask}
+            />
 
         </div>
     );
