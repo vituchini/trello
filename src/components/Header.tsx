fd6363d554e12269efb0d862b8ac569aecdef243
vituchini <petrovichvictorpro99@gmail.com>
2022-11-24 13:54:02 +0200

    feat: add removing of todolists

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import {Button} from './Button';
 
 type PropsType = {
     title: string
+    name: string
+    callback: () => void
 }
 
 export const Header = (props: PropsType) => {
     return (
-        <h3>{props.title}</h3>
+        <h3>{props.title}
+            <Button name={props.name}  callback={props.callback}/>
+        </h3>
     );
 };
