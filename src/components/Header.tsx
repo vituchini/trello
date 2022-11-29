2f805f24c1e86647aa74cc8fd90bd011bd8d495a
vituchini <petrovichvictorpro99@gmail.com>
2022-11-29 22:24:09 +0200

    feat: add ability for todolists modification

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import {Button} from './Button';
+import {EditableSpan} from '../EditableSpan';
 
 type PropsType = {
     title: string
     name: string
     callback: () => void
+    changeTitle: (newTitle: string) => void
 }
 
 export const Header = (props: PropsType) => {
     return (
-        <h3>{props.title}
-            <Button name={props.name}  callback={props.callback}/>
+        <h3>
+            <EditableSpan title={props.title} callback={props.changeTitle}/>
+            <Button name={props.name} callback={props.callback}/>
         </h3>
     );
 };
