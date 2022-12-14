d074bd2e51f9d7eb98d2c8584de1496348e5196d
vituchini <petrovichvictorpro99@gmail.com>
2022-12-14 22:10:44 +0200

    feat: App initializing, routers, redirect, auth/me

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,22 +3,27 @@ import {TodolistsActionsType, todolistsReducer} from '../features/TodolistsList/
 import {applyMiddleware, combineReducers, legacy_createStore as createStore} from 'redux';
 import thunk, {ThunkAction, ThunkDispatch} from 'redux-thunk';
 import {appReducer} from './app-reducer';
+import {AuthActionsType, authReducer} from '../features/Login/auth-reducer';
+import {useDispatch} from 'react-redux';
 
 const rootReducer = combineReducers({
     tasks: tasksReducer,
     todolists: todolistsReducer,
-    app: appReducer
+    app: appReducer,
+    auth: authReducer
 })
 
 export const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
-export type ActionsType = TodolistsActionsType | TasksActionsType
+export type ActionsType = TodolistsActionsType | TasksActionsType | AuthActionsType
 
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppRootStateType, unknown, ActionsType>
 
 export type AppDispatch = ThunkDispatch<AppRootStateType, unknown, ActionsType>
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
 
 // @ts-ignore
 window.store = store;
--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,14 @@ import * as serviceWorker from './serviceWorker';
 import {Provider} from 'react-redux';
 import {store} from './app/store';
 import App from './app/App';
+import {BrowserRouter} from 'react-router-dom';
 
-ReactDOM.render(<Provider store={store}>
-    <App/>
-</Provider>, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
